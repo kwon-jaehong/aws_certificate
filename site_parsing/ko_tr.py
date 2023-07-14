7389c520c7a3ccba1899d27223e1d2b6ae414998
Ubuntu <ubuntu@ip-192-168-40-80.ap-northeast-2.compute.internal>
2023-07-14 08:39:27 +0000

    fix

--- site_parsing/ko_tr.py
+++ site_parsing/ko_tr.py
@@ -1,14 +1,11 @@
 from googletrans import Translator
 import pandas as pd
 from tqdm import tqdm
-
+import time
 
 # Translator 클래스 객체 선언 (translator라는 변수명은 마음대로 정해주면 됨)
-translator = Translator(timeout=80)
+translator = Translator()
 
-
-
-master_df = pd.DataFrame(columns=['exam_category',"exam_type","quiz_id",'quiz_title','most_vote',"correct",'A','B','C','D','E','F','G','H','I'])
 
 
 def None_most_vote(most_vote,correct):
@@ -18,40 +15,62 @@ def None_most_vote(most_vote,correct):
     
     return most_vote
 
-en_ef = pd.read_csv('./T_CLF_quiz_data.csv')
+en_ef = pd.read_csv('./KO_CLF_quiz_data.csv')
 
-for i in tqdm(range(0,len(en_ef))):
+# en_ef['A']=en_ef['A'].astype('string')
+# en_ef['B']=en_ef['B'].astype('string')
+# en_ef['C']=en_ef['C'].astype('string')
+# en_ef['D']=en_ef['D'].astype('string')
+# en_ef['E']=en_ef['E'].astype('string')
+# en_ef['F']=en_ef['F'].astype('string')
+# en_ef['G']=en_ef['G'].astype('string')
+# en_ef['H']=en_ef['H'].astype('string')
+# en_ef['I']=en_ef['I'].astype('string')
 
-    
-    if str(en_ef.loc[i,'quiz_title'])!="nan":
-        en_ef.loc[i,'quiz_title'] = translator.translate(en_ef.loc[i,'quiz_title'], src='en', dest='ko').text
+for i in tqdm(range(848,len(en_ef))):
+
+
+    if str(en_ef.loc[i,'quiz_title'])!="<NA>":
+        en_ef.loc[i,'quiz_title'] = str(translator.translate(str(en_ef.loc[i,'quiz_title']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
         
-    if str(en_ef.loc[i,'A'])!="nan":
-        en_ef.loc[i,'A'] = translator.translate(en_ef.loc[i,'A'], src='en', dest='ko').text
+    time.sleep(0.5)
+    if str(en_ef.loc[i,'A'])!="<NA>":
+        en_ef.loc[i,'A'] = str(translator.translate(str(en_ef.loc[i,'A']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
         
-    if str(en_ef.loc[i,'B'])!="nan":
-        en_ef.loc[i,'B'] = translator.translate(en_ef.loc[i,'B'], src='en', dest='ko').text
+    time.sleep(0.5)
+    if str(en_ef.loc[i,'B'])!="<NA>":
+        en_ef.loc[i,'B'] = str(translator.translate(str(en_ef.loc[i,'B']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
         
-    if str(en_ef.loc[i,'C'])!="nan":
-        en_ef.loc[i,'C'] = translator.translate(en_ef.loc[i,'C'], src='en', dest='ko').text
+    if str(en_ef.loc[i,'C'])!="<NA>":
+        en_ef.loc[i,'C'] = str(translator.translate(str(en_ef.loc[i,'C']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
         
-    if str(en_ef.loc[i,'D'])!="nan":
-        en_ef.loc[i,'D'] = translator.translate(en_ef.loc[i,'D'], src='en', dest='ko').text
+    if str(en_ef.loc[i,'D'])!="<NA>":
+        en_ef.loc[i,'D'] = str(translator.translate(str(en_ef.loc[i,'D']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
         
-    if str(en_ef.loc[i,'E'])!="nan":
-        en_ef.loc[i,'E'] = translator.translate(en_ef.loc[i,'E'], src='en', dest='ko').text
+    if str(en_ef.loc[i,'E'])!="<NA>":
+        en_ef.loc[i,'E'] = str(translator.translate(str(en_ef.loc[i,'E']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
         
-    if str(en_ef.loc[i,'F'])!="nan":
-        en_ef.loc[i,'F'] = translator.translate(en_ef.loc[i,'F'], src='en', dest='ko').text
+    if str(en_ef.loc[i,'F'])!="<NA>":
+        en_ef.loc[i,'F'] = str(translator.translate(str(en_ef.loc[i,'F']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
         
-    if str(en_ef.loc[i,'G'])!="nan":
-        en_ef.loc[i,'G'] = translator.translate(en_ef.loc[i,'G'], src='en', dest='ko').text
+    if str(en_ef.loc[i,'G'])!="<NA>":
+        en_ef.loc[i,'G'] = str(translator.translate(str(en_ef.loc[i,'G']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
         
-    if str(en_ef.loc[i,'H'])!="nan":
-        en_ef.loc[i,'H'] = translator.translate(en_ef.loc[i,'H'], src='en', dest='ko').text
+    if str(en_ef.loc[i,'H'])!="<NA>":
+        en_ef.loc[i,'H'] = str(translator.translate(str(en_ef.loc[i,'H']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
         
-    if str(en_ef.loc[i,'I'])!="nan":
-        en_ef.loc[i,'I'] = translator.translate(en_ef.loc[i,'I'], src='en', dest='ko').text
+    if str(en_ef.loc[i,'I'])!="<NA>":
+        en_ef.loc[i,'I'] = str(translator.translate(str(en_ef.loc[i,'I']).strip(), src='en', dest='ko').text)
+        time.sleep(0.5)
     
     
 

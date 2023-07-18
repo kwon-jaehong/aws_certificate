d8cc8b436d3ffe278319601e6aef57db0ae4fe43
kwon-jaehong <mrjaehong@naver.com>
2023-07-18 17:18:06 +0900

    fix

--- quiz_app/udemy.py
+++ quiz_app/udemy.py
@@ -1,9 +1,10 @@
 
-quiz_code = 338
+quiz_code = 140
 
 print("챕터",int(quiz_code/65)+1," - ",quiz_code%65)
 
-chapter = 6
-c_quiz_code = 38
+chapter = 3
+c_quiz_code = 17
+
 
 print("퀴즈 코드",((chapter-1)*65)+c_quiz_code)

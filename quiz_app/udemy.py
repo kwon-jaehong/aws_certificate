440403ef39781e505209971482c9e0502433dac6
kwon-jaehong <mrjaehong@naver.com>
2023-08-01 15:29:12 +0900

    fix

--- quiz_app/udemy.py
+++ quiz_app/udemy.py
@@ -1,6 +1,6 @@
 
 
-quiz_code = 376
+quiz_code = 172
 
 print("챕터",int(quiz_code/65)+1," - ",quiz_code%65)
 
@@ -9,3 +9,26 @@ c_quiz_code = 29
 
 
 print("퀴즈 코드",((chapter-1)*65)+c_quiz_code)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
